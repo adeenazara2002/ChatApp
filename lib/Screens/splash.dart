bdb668f8e4cb406e5233a4c64bb6fe6d1af265b8
adeenazara2002 <adinazara2001@gmail.com>
2024-08-22 00:07:31 +0500

    Splash Screen

--- lib/Screens/splash.dart
+++ lib/Screens/splash.dart
@@ -12,22 +12,45 @@ class _SplashState extends State<Splash> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-         child: Container(
-          width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height,
-            color: AppColors.splashColor,
-            child:Column(
+        body: SingleChildScrollView(
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        color: AppColors.splashColor,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+
+            // === MAIN LOGO ====
+
+            Row(
               children: [
-                Row(
-                  children: [
-                    Image.asset('assets/images/')
-                  ],
-                )
+                SizedBox(
+                  height: 500,
+                ),
+                Padding(padding: EdgeInsets.only(left: 110)),
+                Image.asset('assets/images/foodie.png')
               ],
-            ) ,
-         ),
-      )   
-       );
+            ),
+
+            // === FOOD LOGO ===
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 270,
+                ),
+                // Padding(padding: EdgeInsets.only(left: 10)),
+                Image.asset('assets/images/foodlogo.png')
+              ],
+            ),
+
+
+            
+          ],
+        ),
+      ),
+    ));
   }
 }
