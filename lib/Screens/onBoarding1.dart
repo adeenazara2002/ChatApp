e2de1be79f9e5cd5c86f76d81cb1af3e2fade899
adeenazara2002 <adinazara2001@gmail.com>
2024-08-21 21:53:31 +0500

    Chat App

---
+++ lib/Screens/onBoarding1.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class OnBoard1 extends StatefulWidget {
+  const OnBoard1({super.key});
+
+  @override
+  State<OnBoard1> createState() => _OnBoard1State();
+}
+
+class _OnBoard1State extends State<OnBoard1> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
