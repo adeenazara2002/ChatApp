29398d2b124501ac7f28b4d36b336f541e9b26e7
adeenazara2002 <adinazara2001@gmail.com>
2024-08-24 02:18:44 +0500

    details Screen

--- lib/Screens/chatDetails.dart
+++ lib/Screens/chatDetails.dart
@@ -63,19 +63,38 @@ class _ChatDetailsState extends State<ChatDetails> {
               ),
 
               // ==== CHAT BLOCKS ====
-
               SizedBox(
                 height: 10,
               ),
+
               Row(
                 children: [
                   Padding(padding: EdgeInsets.only(left: 30)),
                   Container(
-                    height: 80,
-                    width: 300,
+                    height: MediaQuery.of(context).size.height * 0.7,
+                    width: 302,
                     margin: EdgeInsets.only(top: 10),
                     decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(5),
+                      borderRadius: BorderRadius.circular(0),
+                      border: Border.all(
+                        color: AppColors.textFieldColor,
+                      ),
+                    ),
+                    child: Column(
+                      children: [
+                        // ==== CHAT BLOCKS ====
+
+              // SizedBox(
+              //   height: 10,
+              // ),
+              Row(
+                children: [
+                  Container(
+                    height: 80,
+                    width: 300,
+                    // margin: EdgeInsets.only(top: 10),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(0),
                       border: Border.all(
                         color: AppColors.textFieldColor,
                       ),
@@ -135,173 +154,31 @@ class _ChatDetailsState extends State<ChatDetails> {
                   ),
                 ],
               ),
-
-              // 2
-              SizedBox(
-                height: 10,
-              ),
-              Row(
-                children: [
-                  Padding(padding: EdgeInsets.only(left: 30)),
-                  Container(
-                    height: 80,
-                    width: 300,
-                    margin: EdgeInsets.only(top: 10),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(5),
-                      border: Border.all(
-                        color: AppColors.textFieldColor,
-                      ),
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        SizedBox(
-                          height: 10,
-                        ),
-                        Row(
-                          children: [
-                            Padding(padding: EdgeInsets.only(left: 10)),
-                            CircleAvatar(
-                              backgroundImage:
-                                  AssetImage('assets/images/hawkins.png'),
-                              radius: 30,
-                            ),
-                            Padding(padding: EdgeInsets.only(left: 10)),
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  'hawkins',
-                                  style: TextStyle(
-                                    fontSize: 14,
-                                    fontWeight: FontWeight.w600,
-                                    color: AppColors.headingColor,
-                                  ),
-                                ),
-                                Text(
-                                  'Your order just arrived!',
-                                  style: TextStyle(
-                                    fontSize: 12,
-                                    fontWeight: FontWeight.w400,
-                                    color: AppColors.textColor,
-                                  ),
-                                ),
-                              ],
-                            ),
-                            Spacer(),
-                            Padding(
-                              padding: EdgeInsets.only(right: 10),
-                              child: Text(
-                                '16:00',
-                                style: TextStyle(
-                                  fontSize: 12,
-                                  fontWeight: FontWeight.w400,
-                                  color: AppColors.textColor,
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
                       ],
                     ),
                   ),
                 ],
-              ),
+              )
 
-              // 3
+              // SizedBox(
+              //   height: 300,
+              // ),
 
-              SizedBox(
-                height: 10,
-              ),
-              Row(
-                children: [
-                  Padding(padding: EdgeInsets.only(left: 30)),
-                  Container(
-                    height: 80,
-                    width: 300,
-                    margin: EdgeInsets.only(top: 10),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(5),
-                      border: Border.all(
-                        color: AppColors.textFieldColor,
-                      ),
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        SizedBox(
-                          height: 10,
-                        ),
-                        Row(
-                          children: [
-                            Padding(padding: EdgeInsets.only(left: 10)),
-                            CircleAvatar(
-                              backgroundImage:
-                                  AssetImage('assets/images/alex.png'),
-                              radius: 30,
-                            ),
-                            Padding(padding: EdgeInsets.only(left: 10)),
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  'Ieslie Alexander',
-                                  style: TextStyle(
-                                    fontSize: 14,
-                                    fontWeight: FontWeight.w600,
-                                    color: AppColors.headingColor,
-                                  ),
-                                ),
-                                Text(
-                                  'Your order just arrived!',
-                                  style: TextStyle(
-                                    fontSize: 12,
-                                    fontWeight: FontWeight.w400,
-                                    color: AppColors.textColor,
-                                  ),
-                                ),
-                              ],
-                            ),
-                            Spacer(),
-                            Padding(
-                              padding: EdgeInsets.only(right: 10),
-                              child: Text(
-                                '21:00',
-                                style: TextStyle(
-                                  fontSize: 12,
-                                  fontWeight: FontWeight.w400,
-                                  color: AppColors.textColor,
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-
-              SizedBox(
-                height: 300,
-              ),
-
-              Align(
-                alignment: Alignment.bottomCenter,
-                child: Container(
-                  margin: const EdgeInsets.only(left: 10),
-                  width: 120,
-                  height: 5,
-                  decoration: BoxDecoration(
-                    color: AppColors.headingColor,
-                    borderRadius: BorderRadius.circular(20),
-                    border: Border.all(
-                      color: AppColors.headingColor,
-                    ),
-                  ),
-                ),
-              ),
+              // Align(
+              //   alignment: Alignment.bottomCenter,
+              //   child: Container(
+              //     margin: const EdgeInsets.only(left: 10),
+              //     width: 120,
+              //     height: 5,
+              //     decoration: BoxDecoration(
+              //       color: AppColors.headingColor,
+              //       borderRadius: BorderRadius.circular(20),
+              //       border: Border.all(
+              //         color: AppColors.headingColor,
+              //       ),
+              //     ),
+              //   ),
+              // ),
             ],
           ),
         ),
