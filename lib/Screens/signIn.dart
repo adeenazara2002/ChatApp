3dd13c55e345a6ef4317b420fe5a88f36df14935
adeenazara2002 <adinazara2001@gmail.com>
2024-08-24 00:16:52 +0500

    Sign In

--- lib/Screens/signIn.dart
+++ lib/Screens/signIn.dart
@@ -12,225 +12,231 @@ class _SignInState extends State<SignIn> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        color: AppColors.screenColor,
-        child: Stack(
-          children: [
-            // === Pink Background Image ===
-            Image.asset('assets/images/pinkPart.png'),
+      resizeToAvoidBottomInset: false,
+      body: SingleChildScrollView(
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          color: AppColors.screenColor,
+          child: Stack(
+            children: [
+              // === Pink Background Image ===
+              Image.asset('assets/images/pinkPart.png'),
 
-            // === Foodie Image ===
-            Positioned(
-              top: 100,
-              left: 0,
-              right: 0,
-              child: Center(
-                child: Image.asset('assets/images/foodieWhite.png'),
-              ),
-            ),
-
-            // === Text Below Image ===
-            Positioned(
-              top: 200,
-              left: 70,
-              right: 0,
-              child: Text(
-                'Deliver Favourite Food',
-                style: TextStyle(
-                  fontSize: 21,
-                  fontWeight: FontWeight.w600,
-                  color: AppColors.screenColor,
+              // === Foodie Image ===
+              Positioned(
+                top: 100,
+                left: 0,
+                right: 0,
+                child: Center(
+                  child: Image.asset('assets/images/foodieWhite.png'),
                 ),
               ),
-            ),
 
-            // === Form Container ===
-            Positioned(
-              top: 280,
-              left: 10,
-              child: Container(
-                margin: const EdgeInsets.only(left: 20),
-                width: 300,
-                height: 350,
-                decoration: BoxDecoration(
-                  color: AppColors.screenColor,
-                  borderRadius: BorderRadius.circular(20),
-                  border: Border.all(
+              // === Text Below Image ===
+              Positioned(
+                top: 200,
+                left: 70,
+                right: 0,
+                child: Text(
+                  'Deliver Favourite Food',
+                  style: TextStyle(
+                    fontSize: 21,
+                    fontWeight: FontWeight.w600,
                     color: AppColors.screenColor,
                   ),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withOpacity(0.25),
-                      blurRadius: 8,
-                      offset: Offset(0, 4),
-                    ),
-                  ],
                 ),
-                child: Column(
-                  // mainAxisAlignment: MainAxisAlignment.start,
-                  // crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    // Padding(padding: EdgeInsets.only(left: 100)),
-                    SizedBox(
-                      height: 30,
-                    ),
-                    Text(
-                      'Login',
-                      style: TextStyle(
-                          fontWeight: FontWeight.w700,
-                          fontSize: 25,
-                          color: AppColors.headingColor),
-                    ),
-                    // textfield1
+              ),
 
-                    Container(
-                      height: 60,
-                      width: 260,
-                      margin: EdgeInsets.only(top: 10),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        border: Border.all(
-                          color: AppColors.textFieldColor,
-                        ),
-                      ),
-                      child: TextField(
-                        // controller: _searchController,
-                        decoration: InputDecoration(
-                          prefixIcon: Icon(
-                            Icons.person,
-                            color: AppColors.headingColor,
-                            size: 20,
-                          ),
-                          hintText: 'Enter Email',
-                          hintStyle: TextStyle(
-                            color: AppColors.headingColor,
-                            fontFamily: 'Poppins-Regular',
-                            fontSize: 12,
-                            fontWeight: FontWeight.w400,
-                          ),
-                          border: InputBorder.none,
-                          contentPadding: EdgeInsets.symmetric(
-                            vertical: 20.0,
-                            horizontal: 20.0,
-                          ),
-                        ),
-                        style: TextStyle(
-                          color: AppColors.headingColor,
-                        ),
-                      ),
+              // === Form Container ===
+              Positioned(
+                top: 280,
+                left: 10,
+                child: Container(
+                  margin: const EdgeInsets.only(left: 20),
+                  width: 300,
+                  height: 350,
+                  decoration: BoxDecoration(
+                    color: AppColors.screenColor,
+                    borderRadius: BorderRadius.circular(20),
+                    border: Border.all(
+                      color: AppColors.screenColor,
                     ),
-                    // textfield1
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.25),
+                        blurRadius: 8,
+                        offset: Offset(0, 4),
+                      ),
+                    ],
+                  ),
+                  child: Column(
+                    // mainAxisAlignment: MainAxisAlignment.start,
+                    // crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      // Padding(padding: EdgeInsets.only(left: 100)),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      Text(
+                        'Login',
+                        style: TextStyle(
+                            fontWeight: FontWeight.w700,
+                            fontSize: 25,
+                            color: AppColors.headingColor),
+                      ),
+                      // textfield1
 
-                    Container(
-                      height: 60,
-                      width: 260,
-                      margin: EdgeInsets.only(top: 10),
-                      decoration: BoxDecoration(
-                        color: AppColors.textFieldColor,
-                        borderRadius: BorderRadius.circular(10),
-                        border: Border.all(
-                          color: AppColors.textFieldColor,
-                        ),
-                      ),
-                      child: TextField(
-                        // controller: _searchController,
-                        decoration: InputDecoration(
-                          prefixIcon: Icon(
-                            Icons.lock,
-                            color: AppColors.headingColor,
-                            size: 20,
-                          ),
-                          hintText: 'Password',
-                          hintStyle: TextStyle(
-                            color: AppColors.headingColor,
-                            fontFamily: 'Poppins-Regular',
-                            fontSize: 12,
-                            fontWeight: FontWeight.w400,
-                          ),
-                          suffixIcon: Icon(
-                            Icons.remove_red_eye,
-                            color: AppColors.headingColor,
-                            size: 20,
-                          ),
-                          border: InputBorder.none,
-                          contentPadding: EdgeInsets.symmetric(
-                            vertical: 20.0,
-                            horizontal: 20.0,
+                      Container(
+                        height: 60,
+                        width: 260,
+                        margin: EdgeInsets.only(top: 10),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          border: Border.all(
+                            color: AppColors.textFieldColor,
                           ),
                         ),
-                        style: TextStyle(
-                          color: AppColors.headingColor,
-                        ),
-                      ),
-                    ),
-                    // === text ===
-                    // Padding(padding: EdgeInsets.only(left: 80)),
-                    SizedBox(
-                      height: 5,
-                    ),
-                    Row(
-                      children: [
-                        Padding(padding: EdgeInsets.only(left: 175)),
-                        Text(
-                          'Forget Password?',
-                          style: TextStyle(
-                              fontWeight: FontWeight.w400,
+                        child: TextField(
+                          // controller: _searchController,
+                          decoration: InputDecoration(
+                            prefixIcon: Icon(
+                              Icons.person,
+                              color: AppColors.headingColor,
+                              size: 20,
+                            ),
+                            hintText: 'Enter Email',
+                            hintStyle: TextStyle(
+                              color: AppColors.headingColor,
+                              fontFamily: 'Poppins-Regular',
                               fontSize: 12,
-                              color: AppColors.headingColor),
+                              fontWeight: FontWeight.w400,
+                            ),
+                            border: InputBorder.none,
+                            contentPadding: EdgeInsets.symmetric(
+                              vertical: 20.0,
+                              horizontal: 20.0,
+                            ),
+                          ),
+                          style: TextStyle(
+                            color: AppColors.headingColor,
+                          ),
                         ),
-                      ],
+                      ),
+                      // textfield1
+
+                      Container(
+                        height: 60,
+                        width: 260,
+                        margin: EdgeInsets.only(top: 10),
+                        decoration: BoxDecoration(
+                          color: AppColors.textFieldColor,
+                          borderRadius: BorderRadius.circular(10),
+                          border: Border.all(
+                            color: AppColors.textFieldColor,
+                          ),
+                        ),
+                        child: TextField(
+                          // controller: _searchController,
+                          decoration: InputDecoration(
+                            prefixIcon: Icon(
+                              Icons.lock,
+                              color: AppColors.headingColor,
+                              size: 20,
+                            ),
+                            hintText: 'Password',
+                            hintStyle: TextStyle(
+                              color: AppColors.headingColor,
+                              fontFamily: 'Poppins-Regular',
+                              fontSize: 12,
+                              fontWeight: FontWeight.w400,
+                            ),
+                            suffixIcon: Icon(
+                              Icons.remove_red_eye,
+                              color: AppColors.headingColor,
+                              size: 20,
+                            ),
+                            border: InputBorder.none,
+                            contentPadding: EdgeInsets.symmetric(
+                              vertical: 20.0,
+                              horizontal: 20.0,
+                            ),
+                          ),
+                          style: TextStyle(
+                            color: AppColors.headingColor,
+                          ),
+                        ),
+                      ),
+                      // === text ===
+                      // Padding(padding: EdgeInsets.only(left: 80)),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Row(
+                        children: [
+                          Spacer(), // Moves the text to the right
+                          Padding(
+                            padding: EdgeInsets.only(right: 20),
+                            child: Text(
+                              'Forget Password?',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w400,
+                                  fontSize: 12,
+                                  color: AppColors.headingColor),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+
+              // === Text Below Form ===
+              Positioned(
+                bottom: 40,
+                left: 110,
+                child: Column(
+                  children: [
+                    Text(
+                      'Don\'t have an account?',
+                      style: TextStyle(
+                        color: AppColors.headingColor,
+                        fontWeight: FontWeight.w600,
+                        fontSize: 14,
+                      ),
+                    ),
+                    SizedBox(height: 5),
+                    Padding(padding: EdgeInsets.only(left: 10)),
+                    Text(
+                      'REGISTER',
+                      style: TextStyle(
+                        color: AppColors.pinkColor,
+                        fontWeight: FontWeight.w600,
+                        fontSize: 17,
+                      ),
                     ),
                   ],
                 ),
               ),
-            ),
 
-            // === Text Below Form ===
-            Positioned(
-              bottom: 40,
-              left: 110,
-              child: Column(
-                children: [
-                  Text(
-                    'Don\'t have an account?',
-                    style: TextStyle(
-                      color: AppColors.headingColor,
-                      fontWeight: FontWeight.w600,
-                      fontSize: 14,
-                    ),
-                  ),
-                  SizedBox(height: 5),
-                  Padding(padding: EdgeInsets.only(left: 10)),
-                  Text(
-                    'REGISTER',
-                    style: TextStyle(
-                      color: AppColors.pinkColor,
-                      fontWeight: FontWeight.w600,
-                      fontSize: 17,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: Container(
-                margin: const EdgeInsets.only(bottom: 10),
-                width: 120,
-                height: 5,
-                decoration: BoxDecoration(
-                  color: AppColors.headingColor,
-                  borderRadius: BorderRadius.circular(20),
-                  border: Border.all(
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Container(
+                  margin: const EdgeInsets.only(bottom: 10),
+                  width: 120,
+                  height: 5,
+                  decoration: BoxDecoration(
                     color: AppColors.headingColor,
+                    borderRadius: BorderRadius.circular(20),
+                    border: Border.all(
+                      color: AppColors.headingColor,
+                    ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
