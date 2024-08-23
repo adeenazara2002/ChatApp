d1a28c5fbad0247e92aa00f2fa4227e342a09f93
adeenazara2002 <adinazara2001@gmail.com>
2024-08-23 19:07:09 +0500

    Sign In

--- lib/Screens/signIn.dart
+++ lib/Screens/signIn.dart
@@ -1,3 +1,5 @@
+import 'package:chatapp/Screens/signIn.dart';
+import 'package:chatapp/models/colors.dart';
 import 'package:flutter/material.dart';
 
 class SignIn extends StatefulWidget {
@@ -10,6 +12,124 @@ class SignIn extends StatefulWidget {
 class _SignInState extends State<SignIn> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        color: AppColors.screenColor,
+        child: Column(
+          children: [
+            // === Use Stack to overlay images ===
+            Stack(
+              children: [
+                Image.asset('assets/images/pinkPart.png'),
+                Image.asset('assets/images/burgers.png'),
+              ],
+            ),
+            SizedBox(height: 55),
+
+            // === Text without space between them ===
+            Padding(
+              padding: const EdgeInsets.only(left: 10),
+              child: Column(
+                // crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Foodie is Where Your',
+                    style: TextStyle(
+                      color: AppColors.headingColor,
+                      fontWeight: FontWeight.w700,
+                      fontSize: 25,
+                    ),
+                  ),
+                  // Padding(padding: EdgeInsets.only(left: 100)),
+                  SizedBox(
+                    width: 50,
+                  ),
+
+                  Text(
+                    'Comfort Food Resides',
+                    style: TextStyle(
+                      color: AppColors.headingColor,
+                      fontWeight: FontWeight.w700,
+                      fontSize: 25,
+                    ),
+                  ),
+
+                  SizedBox(
+                    width: 50,
+                  ),
+                  Padding(padding: EdgeInsets.only(top: 10)),
+                  Text(
+                    'Enjoy a fast and smooth food delievery at',
+                    style: TextStyle(
+                      color: AppColors.textColor,
+                      fontWeight: FontWeight.w400,
+                      fontSize: 12,
+                    ),
+                  ),
+
+                  //  Padding(padding: EdgeInsets.only(top: 10)),
+                  Text(
+                    'your doorstep',
+                    style: TextStyle(
+                      color: AppColors.textColor,
+                      fontWeight: FontWeight.w400,
+                      fontSize: 12,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                    context, MaterialPageRoute(builder: (context) => SignIn()));
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: AppColors.pinkColor,
+                foregroundColor: AppColors.screenColor,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(5.0),
+                ),
+              ),
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 15.0, horizontal: 40),
+                child: Text(
+                  'Next',
+                  style: TextStyle(
+                      fontSize: 17.0,
+                      fontFamily: 'Poppins-Regular',
+                      fontWeight: FontWeight.w600),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 50,
+            ),
+
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                margin: const EdgeInsets.only(left: 10),
+                width: 120,
+                height: 5,
+                decoration: BoxDecoration(
+                  color: AppColors.headingColor,
+                  borderRadius: BorderRadius.circular(20),
+                  border: Border.all(
+                    color: AppColors.headingColor,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
