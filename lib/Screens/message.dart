1eed3a7f67a0c245439464f3e423a6e5d9d809bf
adeenazara2002 <adinazara2001@gmail.com>
2024-08-24 01:17:19 +0500

    message Screen

---
+++ lib/Screens/message.dart
@@ -0,0 +1,24 @@
+import 'package:chatapp/models/colors.dart';
+import 'package:flutter/material.dart';
+
+class Message extends StatefulWidget {
+  const Message({super.key});
+
+  @override
+  State<Message> createState() => _MessageState();
+}
+
+class _MessageState extends State<Message> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          color: AppColors.screenColor,
+        ),
+      ),
+    );
+  }
+}
